7577df5e6075255a03441b119c82d79aa3eabbdb
cesar <cesyser@gmail.com>
2019-03-20 11:05:24 +0100

    Inicializamos y mostramos tablero

--- src/dam/TresEnRaya.java
+++ src/dam/TresEnRaya.java
@@ -11,15 +11,38 @@ public class TresEnRaya {
     }
 
     //Métodos
-    public void iniciar(){
-
+    private void iniciar(){
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero[0].length; j++) {
+                tablero[i][j] =0;
+            }
+        }
     }
 
     public void mostrar(){
-
+        for (int i = 0; i < tablero.length; i++) {
+            System.out.printf(" |");
+            for (int j = 0; j < tablero[0].length; j++) {
+                char car = ' ';
+                switch(tablero[i][j]){
+                    case 1: car = 'X'; break;
+                    case 2: car = '0'; break;
+                }
+                System.out.printf(" %c ",car);
+            }
+            System.out.println(" |");
+        }
     }
 
     public void jugar(){
+        this.iniciar();
+        for (int i = 0; i < 9; i++) {
+            this.mostrar();
+            turnoJugador();
+        }
+    }
+
+    private void turnoJugador() {
 
     }
 }
