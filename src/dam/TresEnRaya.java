1e570f6312821df83397748e710de801c7ddaa1f
cesar <cesyser@gmail.com>
2019-03-20 10:25:41 +0100

    iniciamos el programa.

---
+++ src/dam/TresEnRaya.java
@@ -0,0 +1,25 @@
+package dam;
+
+public class TresEnRaya {
+
+    //atributos de clase
+    private int [][] tablero = new int [3][3];
+
+    //constructor
+    public TresEnRaya(){
+        this.iniciar();
+    }
+
+    //Métodos
+    public void iniciar(){
+
+    }
+
+    public void mostrar(){
+
+    }
+
+    public void jugar(){
+
+    }
+}
